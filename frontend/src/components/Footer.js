1ae4edef2ea623c7e7e26277678e83145ef2d2cc
K Venkata Siva Reddy <kudamalasivareddy@gmail.com>
2025-06-15 15:55:20 +0530

    fix: remove unused FaHeart import and update experience stats

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaTwitter, FaGlobe, FaHeart } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaTwitter, FaGlobe } from 'react-icons/fa';
 import { personalInfo } from '../data/portfolioData';
 
 const Footer = () => {
